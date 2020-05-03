4410f53f2b7f440b992199775203f0ecace0c0b3
Wilson <p780729@hotmail.com>
2020-05-03 18:52:54 +0800

    Add User register feature
    Install bcryptjs
    Add userController and signup page
    Install body-parser

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const bodyParser = require('body-parser')
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
