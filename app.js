dee1421b9af2ad0af8849f591f3948babacfb780
Wilson <p780729@hotmail.com>
2020-05-03 19:05:04 +0800

    Add check register feature
    Install express-session connect-flash
    Confirm user password and unique email

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express()
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`)
