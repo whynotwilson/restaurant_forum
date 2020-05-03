e91dddee2e88e50f16c890d57154e5c92d927772
Wilson <p780729@hotmail.com>
2020-05-03 20:03:25 +0800

    Add login feature
    Install passport and passport-local
    Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -14,6 +15,9 @@ app.set('view engine', 'handlebars')
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
@@ -26,4 +30,4 @@ app.listen(port, () => {
   console.log(`app is running on port ${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
