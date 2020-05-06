a0db8a0e504cb1b45ac9db0af48c32aa8300bbf1
Wilson <p780729@hotmail.com>
2020-05-07 00:50:49 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,18 +1,34 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
-    let whereQuery = {}
+    let offset = 0
+    const whereQuery = {}
     let categoryId = ''
+    // 計算目前第幾頁
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    // 判斷分類
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
+      // whereQuery["CategoryId"] = categoryId
       whereQuery.CategoryId = categoryId
     }
 
-    Restaurant.findAll({ where: whereQuery, include: Category })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+    Restaurant.findAndCountAll({ where: whereQuery, include: Category, offset: offset, limit: pageLimit })
+      .then(result => {
+        // data for pagination
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(result.count / pageLimit)
+        const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+
+        const data = result.rows.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50),
           categoryName: r.Category.name
@@ -25,8 +41,12 @@ const restController = {
           console.log(categories)
           return res.render('restaurants', {
             restaurants: data,
-            categories: categories,
-            categoryId: categoryId
+            categories,
+            categoryId,
+            page,
+            totalPage,
+            prev,
+            next
           })
         })
       })
