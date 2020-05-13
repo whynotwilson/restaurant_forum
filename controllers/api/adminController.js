0aadb35893fc7442d55efa2fc35da87dcf71b289
Wilson Tseng <p780729@hotmail.com>
2020-05-13 15:21:10 +0800

    Refactor GET /api/admin/restaurants/:id

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -2,9 +2,15 @@ const db = require('../../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 const adminService = require('../../services/adminServices.js')
+
 const adminController = {
   getRestaurants: (req, res) => {
     adminService.getRestaurants(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
   }
--- services/adminServices.js
+++ services/adminServices.js
@@ -9,9 +9,16 @@ const adminService = {
       nest: true,
       include: [Category]
     }).then(restaurants => {
-      callback({ restaurants })
+      callback(restaurants)
+    })
+  },
+
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
+      callback(restaurant)
     })
   }
+
 }
 
 module.exports = adminService
