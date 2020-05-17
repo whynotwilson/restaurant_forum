f886bb3446b6297f18e01f9d70ba0a43487563a4
Wilson <p780729@hotmail.com>
2020-05-18 00:40:41 +0800

    POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -5,6 +5,7 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -17,5 +18,7 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
+
+router.post('/signin', userController.signIn)
 
 module.exports = router
